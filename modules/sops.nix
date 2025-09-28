274776923e17a0655befaa0776b3e5b07e4868fb
ktechmidas <9920871+ktechmidas@users.noreply.github.com>
2025-09-28 13:07:02 +0300

    Enc

--- modules/sops.nix
+++ modules/sops.nix
@@ -11,6 +11,26 @@
         mode = "0600";
         path = "/home/monotoko/.ssh/github.rsa";
       };
+      evo-app-deploy = {
+        owner = "monotoko";
+        mode = "0600";
+        path = "/home/monotoko/.ssh/evo-app-deploy.rsa";
+      };
+      dash-rsa = {
+        owner = "monotoko";
+        mode = "0600";
+        path = "/home/monotoko/.ssh/dash.rsa";
+      };
+      dashdev = {
+        owner = "monotoko";
+        mode = "0600";
+        path = "/home/monotoko/.ssh/dashdev.rsa";
+      };
+      dashevo = {
+        owner = "monotoko";
+        mode = "0600";
+        path = "/home/monotoko/.ssh/dashevo.rsa";
+      };
     };
   };
 }
